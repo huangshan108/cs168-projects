e219646db26889cad807a6b044bdf4b7249603c9
ShanHuang <shan.huang@schoolmint.net>
2014-12-01 02:10:12 -0800

    [proj3b] just need to extract the header from tcp frags

--- Project3/proj3b/firewall.py
+++ Project3/proj3b/firewall.py
@@ -15,6 +15,7 @@ class Firewall:
         self.iface_ext = iface_ext
         self.ip_to_geo = []
         self.rules = []
+        self.log_rules = []
         
         # TODO: Load the firewall rules (from rule_filename) here.
         rule_file = open(config['rule'], "r")
@@ -25,10 +26,14 @@ class Firewall:
                 continue
             stripped_line = line.strip()
             if stripped_line:
-                self.rules.append(stripped_line.upper().split())
+                if stripped_line.upper().split()[0] == "LOG":
+                    self.log_rules.append(stripped_line.upper().split())
+                else:
+                    self.rules.append(stripped_line.upper().split())
             line = rule_file.readline()
         rule_file.close()
         self.rules.reverse()
+        self.log_rules.reverse()
         
         # TODO: Load the GeoIP DB ('geoipdb.txt') as well.
         geoip_file = open("geoipdb.txt", "r")
@@ -42,6 +47,7 @@ class Firewall:
         geoip_file.close()
             
         # TODO: Also do some initialization if needed.
+        self.httplog = open('http.log', 'a')
 
     # @pkt_dir: either PKT_DIR_INCOMING or PKT_DIR_OUTGOING
     # @pkt: the actual data of the IPv4 packet (including IP header)
@@ -126,9 +132,24 @@ class Firewall:
         if doPass == "DROP":
             return
         elif doPass == "DENY":
-            #do deny
+            if protocol == 'TCP':
+                newIPheader = self.consturctIPheaderTCPRST(pkt)
+                TCPpsuedo = self.getTCPPseudoHeader(newIPheader)
+                origTCPheader = protocolHeader
+                newTCPheader = self.constructTCPheader(origTCPheader, pseudoHeader)
+                finalPacket = newIPheader + newTCPheader
+                self.iface_int.send_ip_packet(finalPacket)
+            else: #must be DNS packet
+                DNSheader = protocolHeader[8:]
+                newDNS = self.constructDNS(DNSheader)
+                newUDP = self.constructUDPHeader(protocolHeader, len(newDNS))
+                newIPheader = self.consturctIPheaderDNS(pkt, len(newDNS))
+                finalPacket = newIPheader + newUDP + newDNS
+                self.iface_int.send_ip_packet(finalPacket)
         elif doPass == "LOG":
-            #do log
+            #must be TCP 
+            TCPoffset = ord(protocolHeader[12])
+            HTTPheader = protocolHeader[TCPoffset*4:]
         elif doPass == "PASS":
             if pkt_dir == PKT_DIR_INCOMING:
                 self.iface_int.send_ip_packet(pkt)
@@ -146,30 +167,139 @@ class Firewall:
     
     def getIPSourceAsStr(self, IPheader):
         return socket.inet_ntoa(IPheader[12:16])
+
+    def getIPSourceAsBytes(self, IPheader):
+        return IPheader[12:16]
     
     def getIPProtocol(self, IPheader):
         return ord(IPheader[9])
+
+    def getIPProtocolAsBytes(self, IPheader):
+        return IPheader[9]
         
     def getIPDestAsStr(self, IPheader):
         return socket.inet_ntoa(IPheader[16:20])
-    
+
+    def getIPDestAsBytes(self, IPheader):
+        return IPheader[16:20]
+
     def getTCPSourcePort(self, TCPheader):
         return struct.unpack('!H', TCPheader[0:2])[0]
 
     def setTCPSourcePort(self, TCPheader, port):
-        #port must be integer
-        TCPheader[0:2] = chr(port)
+    #port must be integer
+        TCPheader[0] = chr(port & 0x00ff)
+        TCPheader[1] = chr(port & 0xff00)
         
     def getTCPDestPort(self, TCPheader):
         return struct.unpack('!H', TCPheader[2:4])[0]
 
     def setTCPDestPort(self, TCPheader, port):
-        #sourcePort must be integer
-        TCPheader[2:4] = chr(port)
+    #port must be integer
+        TCPheader[2] = chr(port & 0x00ff)
+        TCPheader[3] = chr(port & 0xff00)
 
     def setTCPRstFlag(self, TCPheader):
-        TCP[13:14] = chr(ord(TCP[13:14]) | 0x00000100)
-    
+        TCP[13:14] = chr(ord(TCP[13:14]) | 0x04)
+
+    def getTCPPseudoHeader(self, IPheader):
+        #SourceIP and DestIP must be in byte format
+        #return None if length is too long
+        #assume TCPheader length is 20
+        psuedoHeader = []
+        source = self.getIPSourceAsBytes(IPheader)
+        dest = self.getIPDestAsBytes(IPheader)
+        psuedoHeader.append(source)
+        psuedoHeader.append(dest)
+        psuedoHeader.append('\x00')
+        psuedoHeader.append(self.getIPProtocolAsBytes(IPheader))
+        psudeoHeader.append(struct.pack('!H', 20))
+        return "".join(psuedoHeader)
+
+    def computeTCPChecksum(self, TCPheader, psuedoHeader):
+        #assumes that payload has been removed from TCPheader
+        if psuedoHeader == None:
+            return None
+        start = 0
+        total = 0
+        while start < len(pseudoHeader):
+            total += struct.unpack('!H', pseudoHeader[start:start+2])[0]
+            start += 2
+        start = 0
+        while start < len(TCPheader)-1:
+            total += struct.unpack('!H', TCPheader[start:start+2])[0]
+            start += 2
+        if len(TCPheader) & 1 != 0:
+            total += ord(TCPheader[start])
+        while total >> 16 != 0:
+            carry = total >> 16
+            result = total & 0xFFFF
+            total = carry + result
+        return ~total
+
+    def copmuteIPChecksum(self, IPheader):
+        #returns checksum as integer
+        #also assumes checksum field of header is 0
+        start = 0
+        while start < len(IPheader)-1:
+            total += struct.unpack('!H', IPheader[start:start+2])[0]
+            start += 2
+        if len(IPheader) % 2 != 0:
+            total += ord(IPheader[start])
+        while total >> 16 != 0:
+            carry = total >> 16
+            result = total & 0xFFFF
+            total = carry + result
+        return ~total
+
+    def consturctIPheaderDNS(self, origIPheader, DNSNumBytes):
+        #creates an IP header for the deny DNS rule
+        header = list(origIPheader[0:20])
+        header[0] = origIPheader[0] 
+        header[1] = chr(5)
+        header[2:4] = struct.pack('!H', 20+8+DNSNumBytes)#20 for ip and 8 for udp
+        header[4:8] = struct.pack('!L', 0)
+        header[8] = struct.pack('!B', 64)
+        header[9] = origIPheader[9]
+        header[10:12] = struct.pack('!H', 0)
+        header[12:16] = origIPheader[16:20]
+        header[16:20] = origIPheader[12:16]
+        header[10:12] = struct.pack('!H', self.copmuteIPChecksum(header))
+        return "".join(header)
+
+    def consturctIPheaderTCPRST(self, origIPheader):
+        #only works for TCP because of length
+        header = list(origIPheader[0:20])
+        header[0] = origIPheader[0] 
+        header[1] = chr(5)
+        header[2:4] = struct.pack('!H', 40)
+        header[4:8] = struct.pack('!L', 0)
+        header[8] = struct.pack('!B', 64)
+        header[9] = origIPheader[9]
+        header[10:12] = struct.pack('!H', 0)
+        header[12:16] = origIPheader[16:20]
+        header[16:20] = origIPheader[12:16]
+        header[10:12] = struct.pack('!H', self.copmuteIPChecksum(header))
+        return "".join(header)
+
+    def constructTCPheader(self, origTCPheader, pseudoHeader):
+        #creates RST TCP packet with empty payload
+        header = list(origTCPheader[0:20])
+        header[0:2] = origTCPheader[2:4]
+        header[2:4] = origTCPheader[0:2]
+        header[4:8] = struct.pack('!L', 1)
+        ackNum = struct.unpack('!L', origTCPheader[4:8])[0]+1
+        header[8:12] = struct.pack('!L', ackNum)
+        header[12] = chr(0x50)
+        header[13] = chr(20)
+        header[14:16] = struct.pack('!H', 0)
+        header[16:18] = struct.pack('!H', 0)
+        header[18:20] = struct.pack('!H', 0)
+        checksum = self.computeTCPChecksum(header,pseudoHeader)
+        header[16:18] = struct.pack('!H', checksum)
+        return "".join(header)
+
+
     def getUDPSourcePort(self, UDPheader):
         return struct.unpack('!H', UDPheader[0:2])[0]
     
@@ -214,8 +344,36 @@ class Firewall:
         except IndexError:
             return None
         return url
-        
-    
+
+    def constructUDPHeader(self, origUDPheader, DNSNumBytes):
+        header = list()
+        header.append(origUDPheader[2:4]) #source port
+        header.append(origUDPheader[0:2]) #dest port
+        header.append(struct.pack('!H', 8+DNSNumBytes)) #length
+        header.append(struct.pack('!H', 0)) #checksum
+        return "".join(header)
+
+
+    def constructDNS(self, DNSheader):
+        header = list(DNSheader[:12])
+        header[2] = chr(ord(DNSheader[2] & 0x80))
+        header[7] = chr(0x01)
+        header[8:10] = struct.pack('!H', 0)
+        header[10:12] = struct.pack('!H', 0)
+        DNSQuestion = DNSheader[12:]
+        DNSQNameBytes = self.getDNSQNameAsBytes(DNSquestion)
+        questionLength = 4 + len(DNSQName)
+        question = list(DNSQuestion[:questionLength])
+        answer = list()
+        answer.append(DNSQNameBytes) #NAME
+        answer.append(struct.pack('!H', 1)) #TYPE
+        answer.append(struct.pack('!H', 1)) #CLASS
+        answer.append(struct.pack('!L', 1)) #TTL
+        answer.append(struct.pack('!H', 4)) #RDLength
+        answer.append(socket.inet_aton('54.173.224.150'))
+        result = "".join(header+question+answer)
+        return result
+
     def getDNSQNameLength(self, DNSQName):
         #input: DNSQName as the result of function to call to getDNSQNameAsBytes()
         return len(DNSQName)
@@ -228,8 +386,49 @@ class Firewall:
         #returns integer of QClass
         return struct.unpack('!H', DNSquestion[DNSQNameLength+2:DNSQNameLength+4])[0]
     
-        
-    # return "PASS", "DROP", "DENY" or "LOG"
+
+    def logHTTP(self, request, response, ext_tcp_ip = None):
+        """
+        sample input:
+
+        request = "GET / HTTP/1.1\nHost: google.com\nUser-Agent: Web-sniffer/1.0.46 (+http://web-sniffer.net/ Accept-Encoding: gzip\nAccept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7 Cache-Control: no-cache\nAccept-Language: de,en;q=0.7,en-us;q=0.3\n"
+
+        response = "HTTP/1.1 301 Moved Permanently\nLocation: http://www.google.com/\nContent-Type: text/html; charset=UTF-8\nDate: Mon, 18 Nov 2013 23:58:12 GMT\nExpires: Wed, 18 Dec 2013 23:58:12 GMT\nCache-Control: public, max-age=2 592000\nServer: gws\nContent-Length: 219\nX-XSS-Protection: 1; mode=block\nX-Frame-Options: SAMEORIGIN Alternate-Protocol: 80:quic\n"
+        """
+        request = request.split('\n')
+        response = response.split('\n')
+        request_line = request[0]
+        response_line = response[0]
+        host_name = ext_tcp_ip
+        method = request_line.split()[0]
+        path = request_line.split()[1]
+        version = request_line.split()[2]
+        status_cdoe = response_line.split()[1]
+        object_size = "-1"
+        for field in request:
+            if field.split()[0] == "Host:":
+                host_name = field.split()[1]
+                break;
+        for field in response:
+            if field.split()[0] == "Content-Length:":
+                object_size = field.split()[1]
+                break;
+
+        self.httplog.write("host_name: %s\nmethod: %s\npath: %s\nversion: %s\nstatus_code: %s\nobject_size: %s\n\n" % (host_name, method, path, version, status_cdoe, object_size))
+        self.httplog.flush()
+
+    # return "LOG" if we need to log this http packet
+    # return None if we don't need to 
+    def scanLogRules(self, dns, ext_ip, host_header = None):
+        dns = dns.upper()
+        for log_rule in self.log_rules:
+            return_msg = self.handleHostname(dns, ext_ip, host_header, log_rule)
+            if return_msg == "not-match":
+                continue
+            else
+                return return_msg
+
+    # return "PASS", "DROP", "DENY"
     # ip_or_dns: if ip, pass in the dot notation, not the ip_to_integer
     def scanRules(self, protocol_type, ip, dns_packet = False, port = None, dns_server = None):
         # print protocol_type
@@ -312,9 +511,24 @@ class Firewall:
             return rule[0]
         return "not-match"
     
-    
-    
-    # sample return: (3360768000, 3360781839, 'AR')
+    def handleHostname(self, dns, ext_ip, host_header, log_rule):
+        match_dns = self.handleDNS(ip_or_dns, log_rule)
+        match_ip = ""
+        if host_header == None:
+            if ext_ip == log_rule[2]
+                match_ip = log_rule[0]
+            else:
+                match_ip = "not-match"
+        else:
+            if host_header == log_rule[2];
+                match_ip = log_rule[0]
+            else:
+                match_ip = "not-match"
+        if match_dns == "LOG" or match_ip == "LOG":
+            return "LOG"
+        return "not-match"
+
+    # sample: (3360768000, 3360781839, 'AR')
     # IPaddress: the integer notation of an ip address
     def geoBinarySearch(self, IPList, IPaddress):
         low = 0
---
+++ Project3/proj3b/log.py
@@ -0,0 +1,51 @@
+httplog = open('http.log', 'a')
+def logHTTP(request, response):
+	request = request.split('\n')
+	response = response.split('\n')
+	# print request
+	# print response
+	request_line = request[0]
+	response_line = response[0]
+	host_name = "external ip addr of TCP connection"
+	method = request_line.split()[0]
+	path = request_line.split()[1]
+	version = request_line.split()[2]
+	status_cdoe = response_line.split()[1]
+	object_size = "-1"
+	for field in request:
+		if field.split()[0] == "Host:":
+			host_name = field.split()[1]
+			break;
+	for field in response:
+		if field.split()[0] == "Content-Length:":
+			object_size = field.split()[1]
+			break;
+
+	httplog.write("host_name: %s\nmethod: %s\npath: %s\nversion: %s\nstatus_code: %s\nobject_size: %s\n\n" % (host_name, method, path, version, status_cdoe, object_size))
+	httplog.flush()
+
+request = "GET / HTTP/1.1\nHost: google.com\nUser-Agent: Web-sniffer/1.0.46 (+http://web-sniffer.net/ Accept-Encoding: gzip\nAccept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7 Cache-Control: no-cache\nAccept-Language: de,en;q=0.7,en-us;q=0.3\n"
+response = "HTTP/1.1 301 Moved Permanently\nLocation: http://www.google.com/\nContent-Type: text/html; charset=UTF-8\nDate: Mon, 18 Nov 2013 23:58:12 GMT\nExpires: Wed, 18 Dec 2013 23:58:12 GMT\nCache-Control: public, max-age=2 592000\nServer: gws\nContent-Length: 219\nX-XSS-Protection: 1; mode=block\nX-Frame-Options: SAMEORIGIN Alternate-Protocol: 80:quic\n"
+
+logHTTP(request, response)
+
+
+rules = []
+log_rules = []
+def load_rules():
+    rule_file = open("rules.conf", "r")
+    line = rule_file.readline()
+    while line != "":
+        if line[0] == "%":
+            line = rule_file.readline()
+            continue
+        stripped_line = line.strip()
+        if stripped_line:
+            if stripped_line.upper().split()[0] == "LOG":
+                log_rules.append(stripped_line.upper().split())
+            else:
+                rules.append(stripped_line.upper().split())
+        line = rule_file.readline()
+    rules.reverse()
+    log_rules.reverse()
+    rule_file.close()
